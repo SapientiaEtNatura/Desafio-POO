0f4f3acc3960e702b21cfaac9f313b4af5bbb999
Carlos Henrique <45887848+SapientiaEtNatura@users.noreply.github.com>
2025-09-04 12:44:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AtvMarkao
+{
+    public class Pessoa 
+    { 
+    
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
